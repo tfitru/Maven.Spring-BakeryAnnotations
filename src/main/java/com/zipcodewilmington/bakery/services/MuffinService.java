0056cc881104bdea456371a688a6a0e189e42ef1
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-08-13 19:31:16 -0400

    Fixed Service

--- src/main/java/com/zipcodewilmington/bakery/services/MuffinService.java
+++ src/main/java/com/zipcodewilmington/bakery/services/MuffinService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-
 public class MuffinService {
     @Autowired
     private MuffinRepository repository;
